e0c33e340aa7fb53d30429ea39bfc8336b644dec
idaira <idairaconejillo@gmail.com>
2024-01-25 15:45:49 +0100

    build paginilla

---
+++ mi_web/.vitepress/config.js
@@ -0,0 +1,16 @@
+export default{
+    lang: 'es-ES',
+    title: "mi we",
+    description: "miwebs persona",
+
+    themeConfig:{
+        footer:{
+            message: "holi mi web perdsonal sisi"
+        },
+        nav:[
+            {text:" inicio", link: "/"},
+            {text:" XAPIADORAS", link: "/"},
+            {text:" ROMANTIC", link: "/"},
+        ]
+    }
+}
---
+++ mi_web/index.md
@@ -0,0 +1,7 @@
+# hola klk manitos
+
+kelokes yose ke tu nome crees
+[pinxa aqui](/archivo1.md)
+
+[queso](/archivo2.md)
+![mi brodi](/download.jpg)
